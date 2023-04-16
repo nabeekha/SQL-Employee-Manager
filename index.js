dd4c6cfefb8d5c529ee4f96cc02af603318a18d7
nabeekha <nabeekha@umich.edu>
2023-04-16 16:08:58 -0400

    creating function to iterate through choices

--- index.js
+++ index.js
@@ -13,3 +13,49 @@ const connection = mysql.createConnection({
     password: 'root'
 })
 
+//setting up initial prompt
+function init(){
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'work',
+            message: 'Please choose what you would like to complete',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'quit']
+        }
+    ])
+    .then((data)=> {
+        //using switch, which is similar to if but uses one value to check across multiple things
+        switch (data.work) {
+            case 'view all departments':
+                viewAllDepartments()
+            break;
+            case 'view all roles':
+                viewAllRoles()
+                break;
+            //repeat for all other steps
+            case 'view all employees':
+                viewAllEmployees()
+            break;
+            case 'add a department':
+                addDept()
+            break;
+            case 'add a role':
+                addRole()
+            break;
+            case 'add an employee':
+                //console.log("test")
+                addEmployee()
+            break;
+            case 'update an employee role':
+                updateRole()
+            break;
+            case 'quit':
+                process.exit()
+            default:
+                //kills command if there are no options selected
+                process.exit()
+        }
+    })
+}
+
